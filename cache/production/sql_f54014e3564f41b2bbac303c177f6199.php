4c0376cd77c746190584408fbac2bc89a4937265
jbaldwin <jbaldwin@coffee.dreamhost.com>
2019-11-18 10:22:44 -0800

    add cache to  gitignore

---
+++ cache/production/sql_f54014e3564f41b2bbac303c177f6199.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1574104712
+SELECT * FROM phpbb_styles s WHERE s.style_id = 18
+320
+a:1:{i:0;a:8:{s:8:"style_id";s:2:"18";s:10:"style_name";s:16:"Flat Style Green";s:15:"style_copyright";s:19:"© Ian Bradley 2017";s:12:"style_active";s:1:"1";s:10:"style_path";s:16:"flat-style-green";s:15:"bbcode_bitfield";s:4:"//g=";s:15:"style_parent_id";s:1:"2";s:17:"style_parent_tree";s:20:"prosilver/flat-style";}}
